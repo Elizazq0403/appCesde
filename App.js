41fbf82d3308ba1bf7a6d3accd9dc59112f7d610
ElizAbeth <lenguaje.pecz@gmail.com>
2023-03-22 23:29:55 -0500

    parte grafica 1

--- App.js
+++ App.js
@@ -1,20 +1,88 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titulo}>CESDE</Text>
+      <Text style={styles.subTitle}>Sistema de Notas</Text>
+      <TextInput
+        placeholder='Identificación:'
+        style={styles.textInput}
+      />
+      <TextInput
+        placeholder='Nombres:'
+        style={styles.textInput}
+      />
+      <TextInput
+        placeholder='Asignatura:'
+        style={styles.textInput}
+      />
+      <TextInput
+        placeholder='Nota 1 (30%):'
+        style={styles.textInput}
+      />
+      <TextInput
+        placeholder='Nota 2 (35%):'
+        style={styles.textInput}
+      />
+      <TextInput
+        placeholder='Nota 3 (35%):'
+        style={styles.textInput}
+      />
+      <TextInput
+        placeholder='Definitiva:'
+        style={styles.textInput}
+      />
+      <TextInput
+        placeholder='Observación:'
+        style={styles.textInput}
+      />
       <StatusBar style="auto" />
     </View>
+    
+
+    
   );
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#e31c79',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  titulo:{
+    fontSize: 60,
+    color: '#ffff',
+    fontWeight: 'bold',
+  },
+  subTitle:{
+    fontSize: 23,
+    color: '#ffff', 
+  },
+  textInput:{
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#000',
+    padding: 5,
+    width: '60%',
+    height: 40,
+    marginTop:20,
+    borderRadius: 30,
+    backgroundColor: '#fff',
+    paddingStart: 20,
+  },
+  buttons:{
+    borderRadius: 30,
+    padding: 5,
+    width: '40%',
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: '#ffff',
+    backgroundColor: '#6f7074',
+  }
 });
